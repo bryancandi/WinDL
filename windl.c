5915c86184be945670202a716452b08bb09881cf
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-02-13 09:02:30 -0700

    Write downloaded content to destination file instead of stdout

--- windl.c
+++ windl.c
@@ -20,10 +20,11 @@
 #define PROTO_FTP_LEN 6
 #define PROTO_SLASH 3
 #define REQUIRED_ARGS 2
+#define MEBIBYTE (1024ULL * 1024ULL)
 
-int open_url(const char *agent, const char *url);
+int download_url(const char *agent, const char *url);
 void print_wininet_error(const char *label);
-unsigned long long get_download_size(HINTERNET hFile);
+unsigned long long get_file_size(HINTERNET hFile);
 
 int main(int argc, char *argv[])
 {
@@ -65,11 +66,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    return open_url(agent, url);
+    return download_url(agent, url);
 }
 
-/* Open a WinINet connection, open the specified URL, and download its contents to stdout. */
-int open_url(const char *agent, const char *url)
+/* Open a WinINet connection, open the specified URL, and download its contents to 'dst' file. */
+int download_url(const char *agent, const char *url)
 {
     HINTERNET hInternet = InternetOpen(agent, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
     if (!hInternet)
@@ -88,36 +89,53 @@ int open_url(const char *agent, const char *url)
         return 1;
     }
 
-    fprintf(stderr, "Loading [%s]\n", url);
+    fprintf(stderr, "Loading URL [%s]\n", url);
 
     /* Advance 'path' past protocol:// in 'url'; check beyond last '/' (if present) to determine filename */
     char *path = strstr(url, "://");
     path += PROTO_SLASH;
     char *fname = strrchr(path, '/');
+    char default_fname[64];
+    long int current_time = time(NULL);
 
     if (!fname || *(fname + 1) == '\0')
     {
-        fprintf(stderr, "Filename not found, using default [download_%li]\n", time(NULL));
+        snprintf(default_fname, sizeof(default_fname), "download_%ld", current_time);
+        fname = default_fname;
+        fprintf(stderr, "Destination [%s] (no filename provided by server, using default)\n\n", fname);
     }
     else
     {
-        fprintf(stderr, "Filename [%s]\n\n", ++fname);
+        fname++;
+        fprintf(stderr, "Destination [%s]\n\n", fname);
     }
 
-    unsigned long long total_size = get_download_size(hFile);
+    unsigned long long total_size = get_file_size(hFile);
     unsigned long long downloaded_size = 0;
 
     char buffer[BUFSIZ];
     DWORD bytesRead = 0;
+    FILE *dst;
+
+    if ((dst = fopen(fname, "wb")) == NULL)
+    {
+        fprintf(stderr, "WinDL: Cannot create destination file '%s'\n", fname);
+        return 1;
+    }
 
     while (InternetReadFile(hFile, buffer, sizeof(buffer), &bytesRead) && bytesRead > 0)
     {
         downloaded_size += bytesRead;
-        fwrite(buffer, 1, bytesRead, stdout);
+        if (fwrite(buffer, 1, bytesRead, dst) != bytesRead)
+        {
+            fprintf(stderr, "WinDL: write error on '%s'\n", fname);
+            fclose(dst);
+            return 1;
+        }
 
         if (total_size > 0)
         {
-            if (downloaded_size % (1024 * 1024) == 0)
+            if (downloaded_size % (MEBIBYTE) == 0)
             {
                 fprintf(stderr, "\rTotal bytes: %llu\tDownloaded bytes: %llu", total_size, downloaded_size);
                 fflush(stderr);
@@ -125,7 +143,7 @@ int open_url(const char *agent, const char *url)
         }
         else
         {
-            if (downloaded_size % (1024 * 1024) == 0)
+            if (downloaded_size % (MEBIBYTE) == 0)
             {
                 fprintf(stderr, "\rTotal bytes: unknown\tDownloaded bytes: %llu", downloaded_size);
                 fflush(stderr);
@@ -133,8 +151,19 @@ int open_url(const char *agent, const char *url)
         }
     }
 
+    /* Print final downloaded file size including the last bytes < 1 MEBIBYTE (1024 * 1024) */
+    if (total_size > 0)
+    {
+        fprintf(stderr, "\rTotal bytes: %llu\tDownloaded bytes: %llu", total_size, downloaded_size);
+    }
+    else
+    {
+        fprintf(stderr, "\rTotal bytes: unknown\tDownloaded bytes: %llu", downloaded_size);
+    }
+
     fprintf(stderr, "\nDownload complete\n");
 
+    fclose(dst);
     InternetCloseHandle(hFile);
     InternetCloseHandle(hInternet);
 
@@ -175,7 +204,7 @@ void print_wininet_error(const char *label)
 }
 
 /* Get file size from hFile */
-unsigned long long get_download_size(HINTERNET hFile)
+unsigned long long get_file_size(HINTERNET hFile)
 {
     char buffer[BUFSIZ];
     DWORD size = sizeof(buffer);
